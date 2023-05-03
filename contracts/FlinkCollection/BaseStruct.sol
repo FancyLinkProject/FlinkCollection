efd22256b60c7b2b65b6ee1cfbeb5bd4eea24b0f
Senn <zone422092678@gmail.com>
2023-05-03 11:04:41 +0800

    isolate struct in FlinkCollection to BaseStruct contract

---
+++ contracts/FlinkCollection/BaseStruct.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+struct TokenInitializationInfo {
+    uint256 tokenId;
+    uint256 version;
+    bytes data;
+    bytes tokenUri;
+    uint256 nonce;
+    bytes signature;
+}
+
+struct TokenInfo {
+    uint256 version;
+    bytes data;
+    bool initialized;
+}
+
+struct SignatureStatus {
+    bool used;
+    bool cancelled;
+}
